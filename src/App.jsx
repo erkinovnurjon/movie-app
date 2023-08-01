4c81e80349dce49506767e48d39f03660a1dc69b
Nurjon <nurjon2477@gmail.com>
2023-08-01 22:01:28 +0500

    added navbar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Movie, SingleMovie, Tv, Home, Single } from "./pages";
+import Header from "./Componenets/MovieCard/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/movie" element={<Movie />} />
